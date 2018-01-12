d3f84c7dee6b92f4907d65186b59872c38ce674a
felipeazv <felipe.azv@gmail.com>
2018-01-12 21:38:25 +0100

    3rd commit

--- src/main/java/com/feazesa/graphql/entity/User.java
+++ src/main/java/com/feazesa/graphql/entity/User.java
@@ -7,7 +7,12 @@ import com.feazesa.graphql.utils.SchemaUtils;
 
 import graphql.annotations.GraphQLField;
 import graphql.annotations.GraphQLName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+@Data
 @GraphQLName(SchemaUtils.USER)
 public class User {
 
@@ -19,50 +24,18 @@ public class User {
     private String email;
     @GraphQLField
     private Integer age;
-
+    
     public User(String name, String email, Integer age) {
-        this.id = genId();
         this.name = name;
         this.email = email;
         this.age = age;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
+    
     public static Long genId() {
         Long id = 1L;
         try {
-            List<User> users =  new UserHandler().getUsers();
+            new UserHandler();
+            List<User> users =  UserHandler.getUsers();
             for (User user : users)
                 id = (user.getId() > id ? user.getId() : id) + 1;
            
